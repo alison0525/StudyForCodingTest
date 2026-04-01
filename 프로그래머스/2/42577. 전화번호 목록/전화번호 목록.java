329fb7ac8692d9b5d5ef457b8764462e5b9897af
최민규 <153361663+alison0525@users.noreply.github.com>
2026-04-01 21:58:20 +0900

    [level 2] Title: 전화번호 목록, Time: 279.66 ms, Memory: 247 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,21 @@
+import java.util.*;
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        
+        Set<String> set = new HashSet<>();
+        
+        for(String s : phone_book){
+            set.add(s);
+        }
+        
+        for (String phone : phone_book) {
+            for (int i = 1; i < phone.length(); i++) {
+                if(set.contains(phone.substring(0,i))){
+                    return false;
+                }
+            }
+        }
+        return answer;
+    }
+}
